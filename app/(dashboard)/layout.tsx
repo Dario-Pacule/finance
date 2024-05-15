87da4738a392b46a2cbdc54d7b7d9fc913feac0b
Dario Pacule <dariopacule@gmail.com>
2024-05-15 14:32:35 +0200

    first commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/header";
+import { QueryProvider } from "@/providers/query-provider";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const DashboardLayout = ({ children }: Props) => {
+  return (
+    <div>
+      <Header />
+      <main className="px-3 lg:px-14">
+        <QueryProvider>{children}</QueryProvider>
+      </main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
